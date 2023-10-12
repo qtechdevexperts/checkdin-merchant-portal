d6a9cb5a2193c68bfce2096f278780142ac49718
mahmed74 <ahmedsheikh0704@gmail.com>
2023-10-12 19:24:41 +0500

    cleanup

--- src/pages/statistics.dashboard.tsx
+++ src/pages/statistics.dashboard.tsx
@@ -5,20 +5,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAngleDown,
   faAngleUp,
-  faGlobeEurope,
   faBuilding,
   faUserAlt,
   faCheckSquare,
 } from "@fortawesome/free-solid-svg-icons";
-import { Row, Col, Card, Button, Table } from "@themesberg/react-bootstrap";
+import { Row, Col, Card, Table } from "@themesberg/react-bootstrap";
 import { renderWindow } from "../services/user.service";
-import { render } from "@testing-library/react";
 
 const Statistics: React.FC = () => {
   const [title, setTitle] = React.useState("ChekdIn Coupons");
   const [headerNum, setHeaderNum] = React.useState("0");
-  const [labelData, setLabelData] = React.useState([]);
-  const [seriesData, setSeriesData] = React.useState([]);
   const [tableFlag, setTableFlag] = React.useState(false);
   const [isMobile, setIsMobile] = React.useState(false);
 
@@ -32,62 +28,7 @@ const Statistics: React.FC = () => {
   }, []);
 
   const SalesValueChart = (chartData: any, chartLabels: any) => {
-    const data = {
-      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-      series: [[0, 0, 0, 0, 0, 0, 0]],
-    };
-
-    const options = {
-      low: 0,
-      showArea: true,
-      fullWidth: true,
-      axisX: {
-        position: "end",
-        showGrid: true,
-      },
-      axisY: {
-        // On the y-axis start means left and end means right
-        showGrid: false,
-        showLabel: false,
-      },
-    };
-
     return <></>;
-  };
-
-  const SalesValueWidgetPhone = (props: any) => {
-    // const { title, value, percentage } = props;
-    const { percentage } = props;
-    const percentageIcon = percentage < 0 ? faAngleDown : faAngleUp;
-    const percentageColor = percentage < 0 ? "text-danger" : "text-success";
-
-    return (
-      <Card className="bg-secondary-alt shadow-sm">
-        <Card.Header className="d-md-flex flex-row align-items-center flex-0">
-          <div className="d-block mb-3 mb-md-0">
-            <h5 className="fw-normal mb-2">{title}</h5>
-            <h3>{headerNum}</h3>
-            <small className="fw-bold mt-2">
-              <span className="me-2">Yesterday</span>
-              <FontAwesomeIcon
-                icon={percentageIcon}
-                className={`${percentageColor} me-1`}
-              />
-              <span className={percentageColor}>{percentage}%</span>
-            </small>
-          </div>
-          <div className="d-flex ms-auto">
-            {/* <Button variant="secondary" size="sm" className="me-2">
-              Month
-            </Button>
-            <Button variant="primary" size="sm" className="me-3">
-              Week
-            </Button> */}
-          </div>
-        </Card.Header>
-        <Card.Body className="p-2"></Card.Body>
-      </Card>
-    );
   };
 
   const SalesValueWidget = (props: any) => {
@@ -165,7 +106,7 @@ const Statistics: React.FC = () => {
   };
 
   const CounterWidget = (props: any) => {
-    const { icon, iconColor, category, title, period, percentage } = props;
+    const { icon, iconColor, category, title, percentage } = props;
     const percentageIcon = percentage < 0 ? faAngleDown : faAngleUp;
     const percentageColor = percentage < 0 ? "text-danger" : "text-success";
 
